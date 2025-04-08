1f4e0e62f61df450212ef9a0f5b68656da1b6b2c
chillag09733 <chillag09733@gmail.com>
2025-04-08 21:20:23 +0200

    javitas

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\CategoryController;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,4 +11,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/tests', [TestController::class, 'index']);
-Route::get('/categories', [TestController::class, 'index']);
+Route::get('/categories', [CategoryController::class, 'index']);
